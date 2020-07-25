f331214b7138a7559e6de60b31282df85f0967cd
Marco Tedone <marco@techwings.io>
2020-07-25 17:28:36 +0100

    Create inputs dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -10,15 +10,27 @@ export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupForm: FormGroup;
 
+  get controls() {
+    return (this.signupForm.get("hobbies") as FormArray).controls;
+  }
+
   ngOnInit() {
     this.signupForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
       gender: new FormControl("male", Validators.required),
+      hobbies: new FormArray([]),
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobbies() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
   }
 }
